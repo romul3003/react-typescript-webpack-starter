b0a0e47d2031b8591b568c4258213935387d7fdd
Roman Zakrevskyi <romul3003@gmail.com>
2022-08-27 15:27:39 +0300

    Add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,18 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: ['interface', 'typeAlias'],
+        format: [
+          'PascalCase',
+        ],
+      },
+    ],
+
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
 
     'import/no-unresolved': [
       'error',
@@ -71,6 +83,8 @@ module.exports = {
     'more/no-numeric-endings-for-variables': 2,
     'more/no-duplicated-chains': 2,
 
+    'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
+
     'react/prop-types': 'off',
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
@@ -87,5 +101,6 @@ module.exports = {
       namedComponents: ['arrow-function', 'function-declaration'],
       unnamedComponents: ['arrow-function'],
     }],
+    'react/require-default-props': [0],
   },
 }
